e068c856b66dc61e5594e29297d7377f0c5a562b
ktwhitehead <ktwhitehead@outlook.com>
2022-12-30 10:18:09 -0500

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import resolve from '@rollup/plugin-node-resolve'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    "global": {},
+    global: {},
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    {
+      ...resolve({
+        preferBuiltins: false,
+        browser: true,
+      }),
+      enforce: 'pre',
+      apply: 'build',
+    },
+    visualizer(),
+  ],
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
 })
